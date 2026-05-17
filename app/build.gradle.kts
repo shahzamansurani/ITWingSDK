2f05a6a10a65956d0aaa8bb6b90981c85cb522ee
itwingtech <shahab.itwing@gmail.com>
2026-05-17 20:05:03 +0500

    Fixes on 17 May

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,15 +18,6 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-
-        val sdkKey = (project.findProperty("ITWING_SDK_KEY") as String?)
-            ?: System.getenv("ITWING_SDK_KEY")
-            ?: "itw_test_example_android_sdk_key_change_me_1234567890"
-        val sdkEndpoint = (project.findProperty("ITWING_SDK_ENDPOINT") as String?)
-            ?: System.getenv("ITWING_SDK_ENDPOINT")
-            ?: "https://itwing.hsgasmart.com/api/sdk/v1"
-        buildConfigField("String", "ITWING_SDK_KEY", "\"$sdkKey\"")
-        buildConfigField("String", "ITWING_SDK_ENDPOINT", "\"$sdkEndpoint\"")
     }
 
     buildTypes {
