e4a9f1faf592e8da789c629785751e8d4697c7aa
itwingtech <shahab.itwing@gmail.com>
2026-05-19 17:12:39 +0500

    Fixes on 17 May

--- itwingSDK/src/main/java/com/itwingtech/itwingsdk/core/ITWingOptions.kt
+++ itwingSDK/src/main/java/com/itwingtech/itwingsdk/core/ITWingOptions.kt
@@ -1,7 +1,7 @@
 package com.itwingtech.itwingsdk.core
 
 data class ITWingOptions(
-    val endpoint: String = "https://itwing.hsgasmart.com/api/sdk/v1",
+    val endpoint: String = "https://sdk.itwingtech.com/api/sdk/v1",
     val bootstrapTimeoutMs: Long = 4_000,
     val strictSslPinning: Boolean = false,
     val analyticsEnabled: Boolean = true,
