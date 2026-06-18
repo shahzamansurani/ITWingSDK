6ecc733676d6843466c5070c1a9f246347c395c3
itwingtech <shahab.itwing@gmail.com>
2026-06-18 20:26:56 +0500

    Release SDK v1.0.1

--- itwingSDK/build.gradle.kts
+++ itwingSDK/build.gradle.kts
@@ -57,7 +57,7 @@ afterEvaluate {
                 from(components["release"])
                 groupId = "com.github.shahzamansurani"
                 artifactId = "ITWingSDK"
-                version = "v1.0"
+                version = "v1.0.1"
             }
         }
     }
